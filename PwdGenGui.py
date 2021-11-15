3733f5db4c0f850665588ebcea2652fbc57ff7f0
dev-delphin <eduard.zakirov.96@mail.ru>
2021-11-15 22:35:56 +0500

    PwdGenGui completed

---
+++ PwdGenGui.py
@@ -0,0 +1,155 @@
+from PyQt5.QtWidgets import QLabel, QRadioButton, QDialog, QWidget, QLineEdit, QButtonGroup, QPushButton, QGridLayout, QApplication
+import sys
+import random
+import string
+import pyperclip as clipboard
+
+class QLabelBuddy(QDialog, QWidget) :
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Password Generator')
+        self.setFixedWidth(250)
+
+        # lovercase
+        lowercaseLabel = QLabel('Lowercase',self)
+        self.lowercaseyes = QRadioButton("Yes")
+        self.lowercaseyes.lovercase = "1"
+        self.lowercaseno = QRadioButton("No")
+        self.lowercaseno.lovercase = "0"
+        
+        # uppercase
+        self.uppercaseLabel = QLabel('Uppercase',self)
+        self.uppercaseyes = QRadioButton(text = "Yes")
+        self.uppercaseyes.uppercase = "1"
+        self.uppercaseno = QRadioButton("No")
+        self.uppercaseno.uppercase = "0"
+        
+        # digits
+        self.digitsLabel = QLabel('Digits',self)
+        self.digitsyes = QRadioButton("Yes")
+        self.digitsyes.digits = "1"
+        self.digitsno = QRadioButton("No")
+        self.digitsno.digits = "0"
+
+        # punctuation
+        self.punctuationLabel = QLabel('Punctuation',self)
+        self.punctuationyes = QRadioButton("Yes")
+        self.punctuationyes.punctuation = "1"
+        self.punctuationno = QRadioButton("No")
+        self.punctuationno.punctuation = "0"
+
+        # Line edit length password
+        self.countLabel = QLabel('Length',self)
+        self.countLineEdit = QLineEdit(self)
+        self.countLabel.setBuddy(self.countLineEdit)
+
+        self.BtnGroupLowercase = QButtonGroup()
+        self.BtnGroupLowercase.addButton(self.lowercaseyes)
+        self.BtnGroupLowercase.addButton(self.lowercaseno)
+
+        self.BtnGroupUppercase = QButtonGroup()
+        self.BtnGroupUppercase.addButton(self.uppercaseyes)
+        self.BtnGroupUppercase.addButton(self.uppercaseno)
+
+        self.BtnGroupDigits = QButtonGroup()
+        self.BtnGroupDigits.addButton(self.digitsyes)
+        self.BtnGroupDigits.addButton(self.digitsno)
+
+        self.BtnGroupPunctuation = QButtonGroup()
+        self.BtnGroupPunctuation.addButton(self.punctuationyes)
+        self.BtnGroupPunctuation.addButton(self.punctuationno)
+
+        mainLayout = QGridLayout(self)
+
+        # lowercase add to window
+        mainLayout.addWidget(lowercaseLabel, 0, 1)
+        mainLayout.addWidget(self.lowercaseyes, 0, 2)
+        mainLayout.addWidget(self.lowercaseno, 0, 3)
+
+        # lowercase add to window
+        mainLayout.addWidget(self.uppercaseLabel, 1, 1)
+        mainLayout.addWidget(self.uppercaseyes, 1, 2)
+        mainLayout.addWidget(self.uppercaseno, 1, 3)
+
+        # digits add to window
+        mainLayout.addWidget(self.digitsLabel, 2, 1)
+        mainLayout.addWidget(self.digitsyes, 2, 2)
+        mainLayout.addWidget(self.digitsno, 2, 3)
+
+        # digits add to window
+        mainLayout.addWidget(self.punctuationLabel, 3, 1)
+        mainLayout.addWidget(self.punctuationyes, 3, 2)
+        mainLayout.addWidget(self.punctuationno, 3, 3)
+
+        # line edit
+        mainLayout.addWidget(self.countLabel,4,1)
+        mainLayout.addWidget(self.countLineEdit,4,2,1,2)
+
+        # generate Button
+        self.generate = QPushButton('Generate', self)
+        mainLayout.addWidget(self.generate, 5, 2)
+
+        # pwd label
+        self.pwd = QLabel("")
+        mainLayout.addWidget(self.pwd, 6, 0, 1,0)
+
+        # button copy
+        self.copy = QPushButton('Copy', self)
+        mainLayout.addWidget(self.copy, 7, 2)
+
+        self.countLineEdit.textChanged.connect(self.textChanged)
+        self.generate.clicked.connect(self.GeneratePwd)
+        self.copy.clicked.connect(self.onClickCopy)
+
+        #diactivate button
+        self.copy.setEnabled(False)
+        self.generate.setEnabled(False)
+
+
+    def textChanged(self, s):
+        if s:
+            self.generate.setEnabled(True)
+        else:
+            self.generate.setEnabled(False)
+
+    def onClickCopy(self):
+        clipboard.copy(self.pwd.text())
+
+    def GeneratePwd(self):
+        lower = string.ascii_lowercase
+        upper = string.ascii_uppercase
+        num = string.digits
+        symbols = string.punctuation
+        all = ""
+        lovercase = self.lowercaseyes.isChecked()
+        uppercase = self.uppercaseyes.isChecked()
+        digits = self.digitsyes.isChecked()
+        punctuation = self.punctuationyes.isChecked()
+        count = int(self.countLineEdit.text())
+        if lovercase == True:
+            all = all + lower
+        else: all = all
+        if uppercase == True:
+            all = all + upper
+        else: all = all
+        if digits == True:
+            all = all + num
+        else: all = all
+        if punctuation == True:
+            all = all + symbols
+        else: all = all
+        length = count
+        temp = random.sample(all,length)
+        password = "".join(temp)
+        self.pwd.setText(password)
+        if len(self.pwd.text()) > 0:
+            self.copy.setEnabled(True)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = QLabelBuddy()
+    main.show()
+    sys.exit(app.exec_())
