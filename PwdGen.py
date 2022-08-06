0375292cce44dd5bd3eb7992b95d17605c1d9b9f
dev-delphin <eduard.zakirov.96@mail.ru>
2022-08-07 00:20:12 +0500

    refactoring

--- PwdGen.py
+++ PwdGen.py
@@ -1,72 +1,106 @@
-import secrets, string
+import secrets, string, sys
 
-lower = string.ascii_lowercase
-upper = string.ascii_uppercase
-num = string.digits
-symbols = string.punctuation
-all = ""
+class Config(object):
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    num = string.digits
+    symbols = string.punctuation
 
 def settings():
     global all
+    all = ""
     flag = False
     while not flag:
-        lc = input("Do use lowercase? (Yes,y/No,n)")
-        if lc == "Yes" or lc == "YEs" or lc == "YES" or lc == "yES" or lc == "yeS" or lc == "yes" or lc == "yEs" or lc == "y" or lc == "Y":
-            all = all + lower
+        lc = input("\nDo use lowercase? (Y,y/N,n)")
+        if lc == "y" or lc == "Y":
+            all = all + Config.lower
             flag = True
-        elif lc == "No" or lc == "NO" or lc == "nO" or lc == "N" or lc == "n":
+        elif lc == "N" or lc == "n":
             all = all
             flag = True
         else: 
             flag = False
-            print ("Incorrect symbol")
+            print ("\nIncorrect symbol\nWrite Y or N")
     flag = False
     while not flag:
-        uc = input("Do use uppercase? (Yes,y/No,n)")
-        if uc == "Yes" or uc == "YEs" or uc == "YES" or uc == "yES" or uc == "yeS" or uc == "yes" or uc == "yEs" or uc == "y" or uc == "Y":
-            all = all + upper
+        uc = input("\nDo use uppercase? (Y,y/N,n)")
+        if uc == "y" or uc == "Y":
+            all = all + Config.upper
             flag = True
-        elif uc == "No" or uc == "NO" or uc == "nO" or uc == "N" or uc == "n":
+        elif uc == "N" or uc == "n":
             all = all
             flag = True
         else: 
             flag = False
-            print ("Incorrect symbol")
+            print ("\nIncorrect symbol\nWrite Y or N")
     flag = False
     while not flag:
-        digit = input("Do use digits? (Yes,y/No,n)")
-        if digit == "Yes" or digit == "YEs" or digit == "YES" or digit == "yES" or digit == "yeS" or digit == "yes" or digit == "yEs" or digit == "y" or digit == "Y":
-            all = all + num
+        digit = input("\nDo use digits? (Y,y/N,n)")
+        if digit == "y" or digit == "Y":
+            all = all + Config.num
             flag = True
-        elif digit == "No" or digit == "NO" or digit == "nO" or digit == "N" or digit == "n":
+        elif digit == "N" or digit == "n":
             all = all
             flag = True
         else: 
             flag = False
-            print ("Incorrect symbol")
+            print ("\nIncorrect symbol\nWrite Y or N")
     flag = False
     while not flag:
-        sumbol = input("Do use punctuation? (Yes,y/No,n)")
-        if sumbol == "Yes" or sumbol == "YEs" or sumbol == "YES" or sumbol == "yES" or sumbol == "yeS" or sumbol == "yes" or sumbol == "yEs" or sumbol == "y" or sumbol == "Y":
-            all = all + symbols
+        sumbol = input("\nDo use punctuation? (Y,y/N,n)")
+        if sumbol == "y" or sumbol == "Y":
+            all = all + Config.symbols
             flag = True
-        elif sumbol == "No" or sumbol == "NO" or sumbol == "nO" or sumbol == "N" or sumbol == "n":
+        elif sumbol == "N" or sumbol == "n":
             all = all
             flag = True
         else: 
             flag = False
-            print ("Incorrect symbol")
+            print ("\nIncorrect symbol\nWrite Y or N")
     return all
 
 def generate(all):
-    length = int(input('\nEnter the length of password: '))
-    password = "".join(secrets.choice(all) for i in range(length)) 
-    print(password)
+    flag = False
+    while not flag:
+        length = input('\nEnter the length of password: ')
+        if not length.isdigit(): #isinstance(length, (int)):
+            flag = False
+        else:
+            length = int(length)
+            password = "".join(secrets.choice(all) for i in range(length)) 
+            print('\nPassword -> %s' % password)
+            flag = True
+
 
     #all = string.ascii_letters + string.digits + string.punctuation
     #passwd = "".join(random.sample(all,length))
     #print(passwd)
 
-print('\nWelcome to Password generator!\n')
-settings()
-generate(all)
+def app():
+    print('\nWelcome to Password generator!')
+    settings()
+    generate(all)
+    flag = False
+    while not flag:
+        new = input('\nGenerate a new password? (Y,y/N,n)')
+        if new == "y" or new == "Y":
+            current = input('\nUse current settings? (Y,y/N,n)')
+            if current == "y" or current == "Y":
+                generate(all)
+            elif current == "N" or current == "n":
+                settings()
+                generate(all)
+            else: 
+                print ("\nIncorrect symbol\nWrite Y or N")
+        elif new == "N" or new == "n":
+            print ("\nExit\n")
+            sys.exit()
+        else: 
+            flag = True
+            print ("\nIncorrect symbol\nWrite Y or N")
+
+
+if __name__ == "__main__":
+    app()
+
+    
